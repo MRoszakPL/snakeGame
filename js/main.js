97bba516ff5f9c42d41f1f3cb1930742cb1b0805
MRoszakPl <roszak.maciej.msg@gmail.com>
2018-07-26 12:11:26 +0200

    Now u can't turn head of snake 180 in one move

--- js/main.js
+++ js/main.js
@@ -1,29 +1,40 @@
-
 document.addEventListener('DOMContentLoaded', function () {
 
     function Snake( variableX) {
 
         this.positionsX = [1,2,3];
         this.positionsY = [0,0,0];
-        this.length = 3 + variableX;
+        this.length = 3 + parseInt(variableX);
         this.direction = 'right';
+        this.lastOppositeDirection = 'left';
 
         this.setDirection = function (direction) {
+
+
             switch (direction) {
                 case 37:
-                    this.direction = 'left';
+                    if('left' !==  this.lastOppositeDirection){
+                        this.direction = 'left';
+                    }
                     break;
 
                 case 38:
-                    this.direction = 'up';
+                    if('up' !==  this.lastOppositeDirection){
+                        this.direction = 'up';
+                    }
                     break;
 
                 case 39:
-                    this.direction = 'right';
+                    if('right' !== this.lastOppositeDirection){
+                        this.direction = 'right';
+                    }
+
                     break;
 
                 case 40:
-                    this.direction = 'down';
+                    if('down' !== this.lastOppositeDirection){
+                        this.direction = 'down';
+                    }
                     break;
             }
         };
@@ -34,30 +45,57 @@ document.addEventListener('DOMContentLoaded', function () {
                 switch(this.direction){
 
                     case "right":
-                        this.positionsX.push(this.positionsX[this.positionsX.length-1]+1);
+                        if(this.positionsX[this.positionsX.length-1] === 39){
+                            this.positionsX.push(0);
+                        } else {
+                            this.positionsX.push(this.positionsX[this.positionsX.length-1]+1);
+                        }
                         this.positionsY.push(this.positionsY[this.positionsY.length-1]);
+                        this.lastOppositeDirection = 'left';
                         break;
 
                     case "left":
-                        this.positionsX.push(this.positionsX[this.positionsX.length-1]-1);
+                        if(this.positionsX[this.positionsX.length-1] === 0){
+                            this.positionsX.push(39);
+                        } else {
+                            this.positionsX.push(this.positionsX[this.positionsX.length-1]-1);
+                        }
                         this.positionsY.push(this.positionsY[this.positionsY.length-1]);
+                        this.lastOppositeDirection = 'right';
                         break;
 
                     case "up":
-                        this.positionsY.push(this.positionsY[this.positionsY.length-1]+1);
+                        if(this.positionsY[this.positionsY.length-1] === 0){
+                            this.positionsY.push(-39);
+                        } else {
+                            this.positionsY.push(this.positionsY[this.positionsY.length-1]+1);
+                        }
                         this.positionsX.push(this.positionsX[this.positionsX.length-1]);
+                        this.lastOppositeDirection = 'down';
                         break;
 
                     case "down":
-                        this.positionsY.push(this.positionsY[this.positionsY.length-1]-1);
+                        if(this.positionsY[this.positionsY.length-1] === -39){
+                            this.positionsY.push(0);
+                        } else {
+                            this.positionsY.push(this.positionsY[this.positionsY.length-1]-1);
+                        }
                         this.positionsX.push(this.positionsX[this.positionsX.length-1]);
+                        this.lastOppositeDirection = 'up';
                         break;
                 }
 
-                this.arraylength = this.positionsX.length;
+
                 console.log(this.positionsX);
                 console.log(this.positionsY);
-                if(this.arraylength > this.length){
+                console.log(this.lastOppositeDirection);
+
+                this.arraylength = this.positionsX.length;
+
+                console.log(this.arraylength);
+                console.log(this.length);
+
+                if(this.arraylength === this.length){
                     this.positionsX = this.positionsX.slice(1,this.arraylength);
                     this.positionsY = this.positionsY.slice(1,this.arraylength);
                 }
@@ -74,6 +112,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
     }
 
+    function Board(level) {
+
+    }
+
     function Apple() {
 
         this.positionX = [];
@@ -82,15 +124,13 @@ document.addEventListener('DOMContentLoaded', function () {
         this.setNewPosition = function(){
             
         }
-
     }
 
     function Game(){
 
-
-
-        this. snake = 0;
-        this.apple = new Apple();
+        this.snake = 0;
+        this.board = 0;
+        this.apple = 0;
 
         const self = this;
         this.time = 0;
@@ -114,11 +154,6 @@ document.addEventListener('DOMContentLoaded', function () {
             }, 1000)
         };
 
-
-        this.stopGame = function () {
-            clearInterval(this.gameTimer);
-        }
-
         this.gameOver = function () {
             clearInterval(this.gameTimer);
         };
@@ -130,6 +165,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const inputX = document.querySelector('#valueX');
     const inputY = document.querySelector('#valueY');
     const speed = document.querySelector('#speed');
+    const level = document.querySelector('#level');
 
     inputX.value = GameVar.settings.X;
     inputY.value = GameVar.settings.Y;
@@ -139,16 +175,32 @@ document.addEventListener('DOMContentLoaded', function () {
 
     startButton.addEventListener('click', function () {
         if(GameVar.stateOfGame === 0) {
-            console.log(GameVar.settings.X);
+
+
+            //Read set settings
+            GameVar.settings.X = inputX.value;
+            GameVar.settings.Y = inputY.value;
+            GameVar.settings.speed = speed.value;
+            GameVar.settings.level = level.value;
+
+
+            //Initialization of game
+            GameVar.board =  new Board(GameVar.settings.level);
             GameVar.snake =  new Snake(GameVar.settings.X);
+            GameVar.apple =  new Apple();
+
             document.addEventListener('keydown', function (event) {
                 GameVar.snake.setDirection(event.keyCode);
             });
+
+            //Start of game
             GameVar.startGame();
+
             startButton.innerHTML = 'Zakończ';
             GameVar.stateOfGame = 1;
+
         } else {
-            GameVar.stopGame();
+            GameVar.gameOver();
             GameVar.stateOfGame = 0;
             startButton.innerHTML = 'Rozpocznij od nowa';
         }
