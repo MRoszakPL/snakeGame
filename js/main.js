d7c32b0657a0bd4e9cf5614b08361c2b3f958118
MRoszakPl <roszak.maciej.msg@gmail.com>
2018-07-27 14:55:23 +0200

    Change the maze creation process

--- js/main.js
+++ js/main.js
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded', function () {
         this.length = 3;
         this.direction = 'right';
         this.lastOppositeDirection = 'left';
-
+        this.color = "rgb(0,128,0)"; //Dark Green
 
         this.setDirection = function (direction) {
 
@@ -139,66 +139,65 @@ document.addEventListener('DOMContentLoaded', function () {
 
         this.obstaclePositionX = [];
         this.obstaclePositionY = [];
+        this.levels = {
+            level1: {posX: [
+                    2,3,4,5,6,7,8,9,
+                    2,3,4,5,6,7,8,9,
+                    2,2,2,2,2,2,2,2,
+                    2,2,2,2,2,2,2,2,
+                    37,37,37,37,37,37,37,37,
+                    37,37,37,37,37,37,37,37,
+                    37,36,35,34,33,32,31,30,
+                    37,36,35,34,33,32,31, 30
+                ],posY: [
+                    -2,-2,-2,-2,-2,-2,-2,-2,
+                    -37,-37,-37,-37,-37,-37,-37,-37,
+                    -3,-4,-5,-6,-7,-8,-9,-10,
+                    -36,-35,-34,-33,-32,-31,-30,-29,
+                    -3,-4,-5,-6,-7,-8,-9,-10,
+                    -36,-35,-34,-33,-32,-31,-30,-29,
+                    -2,-2,-2,-2,-2,-2,-2,-2,
+                    -37,-37,-37,-37,-37,-37,-37,-37]},
+            level2: {
+                posX: [10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,
+                            19,19,19,19,19,20,20,20,20,20, 19,19,19,19,19,20,20,20,20,20],
+                posY: [-19,-19,-19,-19,-19,-19,-19,-19,-19,-19,-19,-19,-19,-19,-19,-19,-19,-19,-19,-19,
+                            -17,-16,-15,-14,-13, -17,-16,-15,-14,-13, -21,-22,-23,-24,-25, -21,-22,-23,-24,-25]},
+            level3: {posX: [ 19,19,19,19,19,20,20,20,20,20, 19,19,19,19,19,20,20,20,20,20,
+                              15,16,17,18,19,20,21,22,23,24, 15,16,17,18,19,20,21,22,23,24],
+                     posY: [-3,-4,-5,-6,-7, -3,-4,-5,-6,-7, -36,-35,-34,-33,-32, -36,-35,-34,-33,-32,
+                            -2,-2,-2,-2,-2,-2,-2,-2,-2,-2,   -37,-37,-37,-37,-37,-37,-37,-37,-37,-37]},
+            level4: {posX: [5,5,5,5,5,5,5, 5,5,5,5,5,5,5,  34,34,34,34,34,34,34, 34,34,34,34,34,34,34],
+                    posY: [-8,-9,-10,-11,-12,-13,-14, -30,-29,-28,-27,-26,-25,-24, -8,-9,-10,-11,-12,-13,-14, -30,-29,-28,-27,-26,-25,-24]},
+            level5: {posX: [10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,
+                            10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29],
+                    posY: [-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,
+                            -28,-28,-28,-28,-28,-28,-28,-28,-28,-28,-28,-28,-28,-28,-28,-28,-28,-28,-28,-28]}
+        }
 
-        //Creating random obstacles on the board
-        switch(parseInt(level)){
-            case 1:
-                for(let i = 0; i<40; i++) {
-                    let positionX = Math.round(Math.random()*(39));
-                    this.obstaclePositionX.push( positionX );
-                    if(positionX > 5){
-                        this.obstaclePositionY.push( Math.round(Math.random()*(-39)));
-                    } else {
-                        this.obstaclePositionY.push( Math.round(Math.random()*(-37))-2);
-                    }
-                }
-                break;
-            case 2:
-                for(let i = 0; i<80; i++) {
-                    let positionX = Math.round(Math.random()*(39));
-                    this.obstaclePositionX.push( positionX );
-                    if(positionX > 5){
-                        this.obstaclePositionY.push( Math.round(Math.random()*(-39)));
-                    } else {
-                        this.obstaclePositionY.push( Math.round(Math.random()*(-37))-2);
-                    }
-                }
-                break;
-            case 3:
+        //Creating obstacles on the board
+        switch(level){
 
-                for(let i = 0; i<120; i++) {
-                    let positionX = Math.round(Math.random()*(39));
-                    this.obstaclePositionX.push( positionX );
-                    if(positionX > 5){
-                        this.obstaclePositionY.push( Math.round(Math.random()*(-39)));
-                    } else {
-                        this.obstaclePositionY.push( Math.round(Math.random()*(-37))-2);
-                    }
-                }
-                break;
-            case 4:
+            case '1':
+                this.obstaclePositionX = this.levels.level1.posX;
+                this.obstaclePositionY = this.levels.level1.posY;
 
-                for(let i = 0; i<160; i++) {
-                    let positionX = Math.round(Math.random()*(39));
-                    this.obstaclePositionX.push( positionX );
-                    if(positionX > 5){
-                        this.obstaclePositionY.push( Math.round(Math.random()*(-39)));
-                    } else {
-                        this.obstaclePositionY.push( Math.round(Math.random()*(-37))-2);
-                    }
-                }
                 break;
-            case 5:
-
-                for(let i = 0; i<200; i++) {
-                    let positionX = Math.round(Math.random()*(39));
-                    this.obstaclePositionX.push( positionX );
-                    if(positionX > 5){
-                        this.obstaclePositionY.push( Math.round(Math.random()*(-39)));
-                    } else {
-                        this.obstaclePositionY.push( Math.round(Math.random()*(-37))-2);
-                    }
-                }
+            case '1.2':
+                this.obstaclePositionX = this.obstaclePositionX.concat(this.levels.level1.posX, this.levels.level2.posX);
+                this.obstaclePositionY = this.obstaclePositionY.concat(this.levels.level1.posY, this.levels.level2.posY);
+                break;
+            case '1.4':
+                this.obstaclePositionX = this.obstaclePositionX.concat(this.levels.level1.posX, this.levels.level2.posX, this.levels.level3.posX);
+                this.obstaclePositionY = this.obstaclePositionY.concat(this.levels.level1.posY, this.levels.level2.posY, this.levels.level3.posY);
+                break;
+            case '1.6':
+                this.obstaclePositionX = this.obstaclePositionX.concat(this.levels.level1.posX, this.levels.level2.posX, this.levels.level3.posX, this.levels.level4.posX);
+                this.obstaclePositionY = this.obstaclePositionY.concat(this.levels.level1.posY, this.levels.level2.posY, this.levels.level3.posY, this.levels.level4.posY);
+                break;
+            case '2':
+                this.obstaclePositionX = this.obstaclePositionX.concat(this.levels.level1.posX, this.levels.level2.posX, this.levels.level3.posX, this.levels.level4.posX,this.levels.level5.posX);
+                this.obstaclePositionY = this.obstaclePositionY.concat(this.levels.level1.posY, this.levels.level2.posY, this.levels.level3.posY, this.levels.level4.posY,this.levels.level5.posY);
                 break;
         }
     }
@@ -250,6 +249,7 @@ document.addEventListener('DOMContentLoaded', function () {
         this.powerUpTimer = 0;
         this.colorOfPowerUp = "rgb(255, 255 , 0)";
         this.specialSetting = 'random';
+
         this.generateProperties = function () {
 
             let i = 0;
@@ -264,8 +264,6 @@ document.addEventListener('DOMContentLoaded', function () {
                         check = true;
                     }
                 }
-
-
 
                 if(check === false) {
                     i=1;
@@ -298,7 +296,7 @@ document.addEventListener('DOMContentLoaded', function () {
                             break;
 
                         case 'invisibility':
-                            this.typeOfPowerUp = 4.5;
+                            this.typeOfPowerUp = 5.5;
                             break;
                 }
             }
@@ -383,7 +381,7 @@ document.addEventListener('DOMContentLoaded', function () {
             Y: 1,
             level: 1,
             speed: 1,
-
+            chosenLevel: 0
         };
 
         this.score = 0;
@@ -398,7 +396,7 @@ document.addEventListener('DOMContentLoaded', function () {
         };
 
         this.renderSnake = function () {
-            context.fillStyle = "rgb(0,128,0)"; //Dark Green
+            context.fillStyle = this.snake.color;
             for(let i = 0; i<this.snake.positionsX.length; i++){
                 context.fillRect(this.snake.positionsX[i]*15,this.snake.positionsY[i]*-15, 15, 15);
             }
@@ -476,9 +474,12 @@ document.addEventListener('DOMContentLoaded', function () {
                         if(this.powerUp.typeOfPowerUp<3 && this.powerUp.typeOfPowerUp>=2){
 
                             this.powerUp.powerUpMuliplier = 2;
+                            this.snake.color = "rgb(255,0,0)";
+
                             this.powerUp.powerUpTimer= setTimeout(
                                 function () {
                                     self.powerUp.powerUpMuliplier = 1;
+                                    self.snake.color = "rgb(0,128,0)"; //Dark Green
                                     clearTimeout(self.powerUp.powerUpTimer);
                                 }
                             , 1000*this.settings.Y);
@@ -488,9 +489,11 @@ document.addEventListener('DOMContentLoaded', function () {
                             if(this.powerUp.typeOfPowerUp<4 && this.powerUp.typeOfPowerUp>=3){
 
                                 this.powerUp.powerUpMuliplier = 0.5;
+                                this.snake.color = "rgb(46,139,87)";
                                 this.powerUp.powerUpTimer= setTimeout(
                                     function () {
                                         self.powerUp.powerUpMuliplier = 1;
+                                        self.snake.color = "rgb(0,128,0)"; //Dark Green
                                         clearTimeout( self.powerUp.powerUpTimer);
                                     }
                                     , 1000*this.settings.Y);
@@ -502,9 +505,11 @@ document.addEventListener('DOMContentLoaded', function () {
                                     console.log('Punkty');
                                 } else {
                                     this.invisible = true;
+                                    this.snake.color = "rgb(192,192,192)";
                                     this.powerUp.powerUpTimer= setTimeout(
                                         function () {
                                             self.invisible = false;
+                                            self.snake.color = "rgb(0,128,0)"; //Dark Green
                                             clearTimeout( self.powerUp.powerUpTimer);
                                         }
                                         , 1000*this.settings.Y);
@@ -609,6 +614,7 @@ document.addEventListener('DOMContentLoaded', function () {
             GameVar.settings.Y = inputY.value;
             GameVar.settings.speed = speed.value;
             GameVar.settings.level = level.value;
+
 
             rewardInfo.innerHTML = 'Podstawa punktowa: '+ (50*GameVar.settings.level*GameVar.settings.speed);
 
